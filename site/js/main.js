3f84c9982e4b47ac43a4eb8f2069c641f3b3a0bb
itggot-malin-larsson <malin.larsson@elev.ga.ntig.se>
2019-05-02 15:27:53 +0200

    Ändrade bilder och fixade bättre parallax

---
+++ site/js/main.js
@@ -0,0 +1,7 @@
+console.log("hello")
+function parallax(){
+    console.log("hello")
+    margin = window.scrollY * 0.8 + "px 0 0 0"
+    
+    document.querySelector(".bg").style.margin = margin
+}
