8b2dc91d08ac1e2ed91829f859917703463b00dc
itggot-malin-larsson <malin.larsson@elev.ga.ntig.se>
2019-05-09 18:26:55 +0200

    Lade till meny,  js funktion till knapp,  content på första sidan, texter + fonts

--- site/js/main.js
+++ site/js/main.js
@@ -1,7 +1,11 @@
 console.log("hello")
 function parallax(){
-    console.log("hello")
     margin = window.scrollY * 0.8 + "px 0 0 0"
     
     document.querySelector(".bg").style.margin = margin
+}
+
+function menu(event){
+    let menu= document.querySelector(".menu")
+    menu.classList.toggle("activatemenu")
 }
